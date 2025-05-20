5479d7e4f44d051e2406c0ed8772e6ad01d29a7a
s21485 <s21485@pjwstk.edu.pl>
2025-05-20 16:01:29 +0200

    Implemented dbContext and part of PrescriptionController.cs

---
+++ WebApplication/Controllers/PrescriptionController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.DTOs;
+
+namespace WebApplication.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class PrescriptionController : ControllerBase
+{
+    private DatabaseContext _context;
+
+    public PrescriptionController(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("Patient/{id}")]
+    public async Task<ActionResult<GetPatientDto>> GetPatientDetails(int id)
+    {
+        var patient = await _context.Patients
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.Doctor)
+            .Include(p => p.Prescriptions)
+            .ThenInclude(pr => pr.Prescription_Medicaments)
+            .ThenInclude(pm => pm.Medicament)
+            .FirstOrDefaultAsync(p => p.IdPatient == id);
+
+        if (patient == null)
+        {
+            return NotFound();
+        }
+
+        var patientDTO = new GetPatientDto
+        {
+            IdPatient = patient.IdPatient,
+            FirstName = patient.FirstName,
+            LastName = patient.LastName,
+            BirthDate = patient.BirthDate,
+            Prescriptions = patient.Prescriptions.OrderBy(p => p.DueDate)
+                .Select(p => new GetPrescriptionDto()
+                {
+                    IdPrescription = p.IdPrescription,
+                    Date = p.Date,
+                    DueDate = p.DueDate,
+                    Doctor = new DoctorDto
+                    {
+                        IdDoctor = p.Doctor.IdDoctor,
+                        FirstName = p.Doctor.FirstName,
+                        LastName = p.Doctor.LastName,
+                    },
+                    Medicaments = p.Prescription_Medicaments
+                        .Select(pm => new GetMedicamentDto
+                        {
+                            IdMedicament = pm.Medicament.IdMedicament,
+                            Name = pm.Medicament.Name,
+                            Description = pm.Medicament.Description,
+                            Dose = pm.Dose
+                        }).ToList()
+                }).ToList()
+        };
+        return Ok(patientDTO);
+    }
+
+}
--- WebApplication/Data/DatabaseContext.cs
+++ WebApplication/Data/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
 
 namespace WebApplication.Data;
 
@@ -6,6 +7,38 @@ public class DatabaseContext : DbContext
 {
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
     
-    
-    
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Medicament> Medicaments { get; set; }
+    public DbSet<Prescription> Prescriptions { get; set; }
+    public DbSet<Prescription_Medicament> Prescription_Medicaments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
+
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasOne(pm => pm.Medicament)
+            .WithMany(pm => pm.Prescription_Medicaments)
+            .HasForeignKey(pm => pm.IdMedicament);
+        
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasOne(pm => pm.Prescription)
+            .WithMany(pm => pm.Prescription_Medicaments)
+            .HasForeignKey(pm => pm.IdPrescription);
+        
+        modelBuilder.Entity<Prescription>()
+            .HasOne(pm => pm.Patient)
+            .WithMany(pr => pr.Prescriptions)
+            .HasForeignKey(pm => pm.IdPatient);
+        
+        modelBuilder.Entity<Prescription>()
+            .HasOne(pm => pm.Doctor)
+            .WithMany(pr => pr.Prescriptions)
+            .HasForeignKey(pm => pm.IdDoctor);
+    }
+
 }
