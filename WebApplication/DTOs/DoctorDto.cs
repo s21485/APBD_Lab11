8d3a9ab4c8947d1f545659071316d4eca06e2d8a
s21485 <s21485@pjwstk.edu.pl>
2025-05-20 10:33:11 +0200

    Added DoctorDto and PatientDto

---
+++ WebApplication/DTOs/DoctorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.DTOs;
+
+public class DoctorDto
+{
+    public int IdDoctor { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
---
+++ WebApplication/DTOs/PatientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication.DTOs;
+
+public class PatientDto
+{
+    public int IdPatient  { get; set; }
+    public string FirstName  { get; set; }
+    public string LastName  { get; set; }
+    public DateTime BirthDate  { get; set; }
+}
